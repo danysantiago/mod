b47368439ac7d244670d6d43b1fc2c07d819b950
danysantiago <daniel.santiago5@upr.edu>
2013-08-22 15:28:44 -0400

    Changed database engine to mysql instead of postgre

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 var config = require("./lib/config.js"),
 	express = require("express"),
 	app = express(),
-	pg = require("pg");
+	mysql = require("mysql");
 
 app.configure(function() {
 	app.set("name", config.appName);
 });
 
-dbClient = new pg.Client(config.dbAddress);
+dbClient = mysql.createConnection(config.dbAddress);
 dbClient.connect(function (err) {
     if(err) {
-        return console.error("Could not connect to postgres", err);
+        return console.error("Could not connect to mysql", err);
     }
 
     console.log("Database connection sucessful");
