d1a465a81c123b2645af14469039a0c62123aeb4
danysantiago <daniel.santiago5@upr.edu>
2013-09-20 20:44:32 -0400

    Added http wrapper

--- app/src/edu/uprm/mod/store/ExampleFragment.java
+++ app/src/edu/uprm/mod/store/ExampleFragment.java
@@ -1,6 +1,9 @@
 package edu.uprm.mod.store;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.security.InvalidParameterException;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -13,6 +16,8 @@ import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import edu.uprm.mod.store.HttpRequest.HttpCallback;
 
 import android.content.Context;
 import android.os.AsyncTask;
@@ -46,80 +51,27 @@ public class ExampleFragment extends Fragment {
 		
 		responseTextView = (TextView) getActivity().findViewById(R.id.response_textView);
 		
-		new HttpRequest().execute();
-	}
-	
-	private class HttpRequest extends AsyncTask<Void, Void, Void>{
-		
-		private DefaultHttpClient client;
-		
-		private boolean success;
-		private String errorMsg;
-		
-		private String exampleMsg;
-		
-		@Override
-		protected void onPreExecute() {
-			// We initialize things here
+		Bundle httpParams = new Bundle();
+		httpParams.putString("method", "GET");
+		httpParams.putString("url", Server.BASE_URL + "/example");
+		HttpRequest request = new HttpRequest(httpParams, new HttpCallback() {
 			
-			// We specify connection parameters, timeout
-			int timeout = 10000;
-			HttpParams httpParameters = new BasicHttpParams();
-			HttpConnectionParams.setConnectionTimeout(httpParameters, timeout);
-			HttpConnectionParams.setSoTimeout(httpParameters, timeout);
-
-			client = new DefaultHttpClient(httpParameters);
-			
-		}
-		
-		@Override
-		protected Void doInBackground(Void... params) {
-			// Background method, request is processed here
-			try {
-			
-				HttpGet get = new HttpGet(Server.BASE_URL + "/example");
-				HttpResponse response = client.execute(get); //Execute get
-				
-				int statusCode = response.getStatusLine().getStatusCode(); // Get http status code
-				
-				if(statusCode == HttpStatus.SC_OK){
-					String fullRes = EntityUtils.toString(response.getEntity()); // Get response full string
-					
-					JSONObject json = new JSONObject(fullRes);
-					exampleMsg = json.getString("message");
-					
-					success = true;
-				} else {
-					success = false;
+			@Override
+			public void onSucess(JSONObject json) {
+				try {
+					responseTextView.setText(json.getString("message"));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-			
-			} catch (JSONException e) {
-				Log.d("JSONLog", "Error Example JSON object", e);
-			} catch (ClientProtocolException e) {
-				success = false;
-				Log.d("BackendEventLog", "Error Example GET", e);
-			} catch (IOException e) {
-				success = false;
-				Log.d("BackendEventLog", "Error Example GET", e);
 			}
 			
-			return null;
-			
-		}
-		
-		@Override
-		protected void onPostExecute(Void result) {
-			//We clean up things here, we also run GUI code here because this method is run by the GUI's thread
-			//else we would get security error
-			
-			if(success){ 
-				responseTextView.setText(exampleMsg);
-			} else {
-				//Something went wrong, show toast message
-				Toast.makeText(context, errorMsg, Toast.LENGTH_SHORT).show();
+			@Override
+			public void onFailed() {
+				Toast.makeText(getActivity(), "Something bad happened,  http-wise.", Toast.LENGTH_SHORT).show();
 			}
-		}
-		
+		});
+		request.execute();
 	}
 
 }
