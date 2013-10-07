b5a54896c21b9df5b77d5b482c26b016d7eb4600
Daniel Santiago <daniel.santiago5@upr.edu>
2013-10-06 22:08:04 -0400

    Added cheap main

--- app/src/icom5016/modstore/fragments/MainFragment.java
+++ app/src/icom5016/modstore/fragments/MainFragment.java
@@ -1,14 +1,24 @@
 package icom5016.modstore.fragments;
 
+import java.util.Random;
+
 import icom5016.modstore.activities.R;
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainFragment extends Fragment {
 
+	private ListView list;
+	
+	Random r = new Random();
+	
 	public MainFragment(){
 	};
 	
@@ -18,33 +28,44 @@ public class MainFragment extends Fragment {
 		View view = inflater.inflate(R.layout.fragment_main, container,false);
 		return view;
 	}
-	/*
+	
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
-	  super.onViewCreated(view, savedInstanceState);
-	  
-	  Bundle params = new Bundle();
-    params.putString("url", Server.BASE_URL + "/example");
-    params.putString("method", "GET");
-    HttpRequest request = new HttpRequest(params, new HttpCallback() {
-      
-      @Override
-      public void onSucess(JSONObject json) {
-        TextView txt = (TextView) getActivity().findViewById(R.id.main_textview);
-        try {
-          txt.setText(json.getString("message"));
-        } catch (JSONException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }
-        
-      }
-      
-      @Override
-      public void onFailed() {
-        // TODO Auto-generated method stub
-        
-      }
-    });
-	}*/
+		// TODO Auto-generated method stub
+		super.onViewCreated(view, savedInstanceState);
+		
+		list = (ListView) view.findViewById(R.id.listView1);
+		
+		MainAdapter adapter = new MainAdapter(getActivity(), R.layout.listview_home_row_2);
+		for(int i = 0; i < 10; i++) {
+			adapter.add(r.nextInt(2));
+		}
+		
+		list.setAdapter(adapter);
+
+	}
+	
+	public class MainAdapter extends ArrayAdapter<Integer> {
+
+		public MainAdapter(Context context, int resource) {
+			super(context, resource);
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			
+			LayoutInflater inflater = ((Activity) this.getContext()).getLayoutInflater();
+
+			View view;
+			
+			if(this.getItem(position) == 0) {
+				view = inflater.inflate(R.layout.listview_home_row_1, parent,false);
+			} else {
+				view = inflater.inflate(R.layout.listview_home_row_2, parent,false);
+			}
+			
+			return view;
+		}
+		
+	}
 }
