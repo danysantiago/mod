f90a8d123544456ece16da4cb1bde89d92bfbf91
mamanu <memarquez01@gmail.com>
2013-11-09 20:38:19 -0400

    Search Almost Completed

--- app/src/icom5016/modstore/resources/AndroidResourceFactory.java
+++ app/src/icom5016/modstore/resources/AndroidResourceFactory.java
@@ -1,9 +1,17 @@
 package icom5016.modstore.resources;
 
+import icom5016.modstore.models.Category;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.TimeZone;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.Fragment;
@@ -42,4 +50,35 @@ public class AndroidResourceFactory {
 		SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy KK:mma");
 		return df.format(date);
 	}
+	
+	public static List<Category> sortCategories(List<Category> unsorted) {
+		List<Category> sorted = new ArrayList<Category>();
+		getCategoriesRecurv(unsorted, sorted, 0, -1);
+		return sorted;
+	}
+	
+	private static void getCategoriesRecurv(List<Category> unsorted, List<Category> cats, int level, int lookId) {
+
+		String name;
+		
+		for(Category e: unsorted){
+			if (e.getParentId() == lookId){
+				
+				name = repeat("   ", level) + e.getName();
+				cats.add(new Category(e.getParentId(), e.getId(), name));
+				getCategoriesRecurv(unsorted, cats, level + 1,e.getId());
+			}
+		}
+	}
+	
+	private static String repeat(String c, int n) {
+		String out = "";
+		
+		for (int i = 0; i < n; i++) {
+			out += c;
+		}
+		
+		return out;
+	}
+	
 }
