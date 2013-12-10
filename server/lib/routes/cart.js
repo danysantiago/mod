c14b71aa291b9e94976fd0d5883a4241adbb0e58
omarpr <omarpr@gmail.com>
2013-12-10 16:30:39 -0400

    Update to Cart Element Route is now working.

--- server/lib/routes/cart.js
+++ server/lib/routes/cart.js
@@ -106,4 +106,60 @@ routes.post("/cart", express.bodyParser(), function (req, res, next) {
   });
 });
 
+routes.put("/cart", express.bodyParser(), function (req, res, next) {
+  var userId = req.body.userId;
+  var productId = req.body.productId;
+  var quantity = req.body.quantity;
+
+  if (userId == undefined) {
+    res.send(404, {"status": "NO_USER"});
+    return;
+  } else if (productId == undefined) {
+    res.send(404, {"status": "NO_PRODUCT"});
+    return;
+  } else if (quantity == undefined) {
+    res.send(404, {"status": "NO_QUANTITY"});
+    return;
+  } else if (quantity <= 0) {
+    res.send(404, {"status": "INVALID_QUANTITY"});
+    return;
+  }
+
+  var query = "SELECT *, (P.quantity - IFNULL((SELECT SUM(quantity) FROM order_detail OD WHERE OD.product_id = P.product_id GROUP BY OD.product_id),0)) AS stock FROM product P WHERE P.product_id = " + productId;
+  console.log("MySQL Query: " + query);
+
+  req.db.query(query, function (err, result) {
+    if (result.length == 0) {
+      res.send(404, {"status": "INVALID_PRODUCT"});
+      return;
+    }
+
+    var product = result[0];
+
+    if ((product.stock - quantity) <= 0) {
+      res.send(404, {"status": "OUT_OF_STOCK"});
+      return;
+    } else if (product.user_id == userId) {
+      res.send(404, {"status": "PRODUCT_FROM_BUYER"});
+      return;
+    }
+
+    var query = "UPDATE cart SET quantity = " + req.db.escape(quantity) + " WHERE user_id = " + req.db.escape(userId) + " AND product_id = " + req.db.escape(productId);
+    console.log("MySQL Query: " + query);
+
+    req.db.query(query, function (err, result) {
+      if (err) {
+          console.log(err);
+          res.send(404, {"status": ((err.code == "ER_NO_REFERENCED_ROW_") ? "FOREIGN_FAIL" : "DB_ERR")});
+          return;
+      }
+      if(result.affectedRows <= 0){
+        res.send(404, {"status": "NO_UPDATED"});
+        return;
+      }
+      res.send(200, {"status": "ok"});
+    });
+  });
+});
+
 module.exports = routes;
