dba53a6d3fd7c9e283cf05823a621f78d58987ff
Manuel Marquez <memarquez01@gmail.com>
2013-12-11 15:09:43 -0400

    CheckoutFragment

--- app/src/icom5016/modstore/http/Server.java
+++ app/src/icom5016/modstore/http/Server.java
@@ -2,8 +2,8 @@ package icom5016.modstore.http;
 
 public class Server {
 	
-	public static final String BASE_URL = "http://192.168.1.104:8080";
-	//public static final String BASE_URL = "http://ec2-54-226-36-4.compute-1.amazonaws.com:8080";
+	//public static final String BASE_URL = "http://192.168.1.104:8080";
+	public static final String BASE_URL = "http://ec2-54-226-36-4.compute-1.amazonaws.com:8080";
 
 	public static class User {
 		public static final String LOGIN = BASE_URL+"/login";
